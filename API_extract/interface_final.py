572c3d136137b5e36fcf08f1b5a48eb857b8eda2
QuinVIVER <84405674+QuinVIVER@users.noreply.github.com>
2022-06-29 21:02:16 +0800

    Update interface_final.py

--- API_extract/interface_final.py
+++ API_extract/interface_final.py
@@ -9,12 +9,15 @@ def main():
               #response = urllib.request.urlopen(url)
               #interface_html = response.read()
               with open(url,'r',encoding='utf-8') as f:
-                     content = f.read()
+                     try:
+                            content = f.read()
+                     except:
+                            continue
               #interface_html = content.decode("utf-8")
               interface_html = str(content)
               #print(interface_html)
 
-              # TODO: 改re为htmlparser
+              
               print("{")
               package_content = re.findall('<div class="header">(.*?)<div class="contentContainer">',interface_html,re.S)
               p = re.compile(r'<div class="subTitle">(.*?)</div>')
