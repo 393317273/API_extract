7ae58e5d5dcc546efb823cd51f9cd452e2542d84
QuinVIVER <84405674+QuinVIVER@users.noreply.github.com>
2022-06-29 21:19:42 +0800

    merge master

--- API_extract/interface_final.py
+++ API_extract/interface_final.py
@@ -2,34 +2,39 @@ import re
 from urllib import response
 import urllib.request
 import interface_link
+import class_link
+
+
+def merge(name1, name2,name3,name4,name5,name6):
+    dict1 = {"Package":name1,"Interface":name2,"Superinterface":name3,
+                "Implementing Class":name4,"Subinterface":name5,"Method":name6}
+    with open('C:/Users/LGH/Desktop/API_extract.txt', 'a+') as f:
+            print(dict1,"\n", file=f)
 
 def main():
        for m1 in range(len(interface_link.all_interface_link)):
               url = interface_link.all_interface_link[m1]
-              #response = urllib.request.urlopen(url)
-              #interface_html = response.read()
               with open(url,'r',encoding='utf-8') as f:
                      try:
-                            content = f.read()
+                            interface_html = f.read()
                      except:
                             continue
-              #interface_html = content.decode("utf-8")
-              interface_html = str(content)
               #print(interface_html)
-
               
-              print("{")
+              #print("{")
               package_content = re.findall('<div class="header">(.*?)<div class="contentContainer">',interface_html,re.S)
               p = re.compile(r'<div class="subTitle">(.*?)</div>')
               for m2 in p.findall(str(package_content)): 
-                     print("Package:")
-                     print(m2)
+                     111
+                     #print("Package:")
+                     #print(m2)
 
 
               p = re.compile(r'<h2 title="Interface(.*)" class="title">', re.MULTILINE)
               for m3 in p.findall(interface_html):
-                     print("Interface:")
-                     print(m3)
+                     111
+                     #print("Interface:")
+                     #print(m3)
 
               
               #------------------Superinterfaces:---------------------------------
@@ -45,12 +50,12 @@ def main():
               for j in p.findall(str(content_interface)):  
                      imple_interface2.append(j)
                      #print(imple_interface2[1])
-              print("Superinterfaces:")
+              #print("Superinterfaces:")
               imple_interface3 = list()
               for l in range(len(imple_interface1)):
                      k = imple_interface1[l] + '.' +imple_interface2[l]
                      imple_interface3.append(k)
-              print(imple_interface3)
+              #print(imple_interface3)
 
 
               #------------------Subinterfaces:---------------------------------
@@ -66,12 +71,12 @@ def main():
               for j in p.findall(str(content_interface)):  
                      imple_interface2.append(j)
                      #print(imple_interface2[1])
-              print("Subinterfaces:")
-              imple_interface3 = list()
+              #print("Subinterfaces:")
+              imple_interface31 = list()
               for l in range(len(imple_interface1)):
                      k = imple_interface1[l] + '.' +imple_interface2[l]
-                     imple_interface3.append(k)
-              print(imple_interface3)
+                     imple_interface31.append(k)
+              #print(imple_interface31)
 
               #------------------Implementing Classes:---------------------------------
               content_class = re.findall('<dt>All Known Implementing Classes:</dt>(.*?)</dl>',interface_html,re.S)
@@ -86,12 +91,12 @@ def main():
               for j in p.findall(str(content_class)):  
                      imple_class2.append(j)
                      #print(imple_interface2[1])
-              print("implement classes:")
+              #print("implement classes:")
               imple_class3 = list()
               for l in range(len(imple_class1)):
                      k = imple_class1[l] + '.' +imple_class2[l]
                      imple_class3.append(k)
-              print(imple_class3)
+              #print(imple_class3)
 
 
               #------------------Method Detail:---------------------------------
@@ -102,30 +107,27 @@ def main():
               for j in p.findall(content):
                      menthon_list1.append(j)
               for x1 in range(len(menthon_list1)):
-                     print("Methon:")
+                     #print("Methon:")
                      methon = re.findall(r'<h4>(.*?)</h4>',menthon_list1[x1])
-                     print(methon)
+                     #print(methon)
 
-                     print("Parameter:")
+                     #print("Parameter:")
                      Parameter_content1 = re.findall(r'<pre>.*\((.*?)\)',menthon_list1[x1])
                      #print(Parameter_content1)
                      Parameter = re.findall(r'title="class in(.*?)">(.*?)</a>&nbsp;(.*?),''',str(Parameter_content1))
-                     print(Parameter)
+                     #print(Parameter)
                      
 
-                     print("Throw:")
+                     #print("Throw:")
                      throw_content = re.findall(r'<dt><span class="throwsLabel">(.*?)</dl>',menthon_list1[x1])
                      throw = re.findall(r'<dd><code><a href=".+">(.*?)</a></code>',str(throw_content))
-                     print(throw)
-                     '''throw_content = re.findall(r'<dt><span class="throwsLabel">(.*?)</dl>',menthon_list1[x1])
-                     p = re.compile('\w.*</a></code>',re.S)
-                     for j in p.findall(content):
-                     print(j)'''
+                     #print(throw)
                      
-              print("}")
-              print("\n")
-if __name__ == "__main__":
-       main()
+                     dict_method = dict()
+                     dict_method = {"method":methon,"Parameter":Parameter,"Throw":throw}
+              merge(m2,m3,imple_interface3,imple_interface31,imple_class3,dict_method)
+
+
    
 
     
